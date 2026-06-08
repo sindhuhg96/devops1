2bd5ca31d0cb5074b16a146980d3ee88c8d3adb2
Thungesh S <sindhuhg96@gmail.com>
2026-06-08 08:49:04 +0530

    shellscript files

---
+++ add.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+
+echo "enter first num1"
+read num1
+echo "enter sec num2"
+read num2
+sum=$((num1 + num2))
+echo "sum of two numbers = $sum"
+
+
---
+++ vote.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $1 -ge 18 ]
+then
+echo "elegible for voting"
+else
+echo "not elegible"
+fi
+
